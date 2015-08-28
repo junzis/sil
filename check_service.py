03baefe78e0342122b5df0c951eecafc351790eb
Junzi Sun <junzi.sun@gmail.com>
2015-08-28 17:53:24 +0200

    fix file path

--- check_service.py
+++ check_service.py
@@ -3,5 +3,5 @@ import sys
 import subprocess
 
 if not os.path.isfile('/tmp/sil-adsb-client.pid'):
-	subprocess.call([sys.executable, 'sil-client-service.py', 'start'])
+	subprocess.call([sys.executable, os.path.dirname(os.path.realpath(__file__))+'/sil-client-service.py', 'start'])
 
