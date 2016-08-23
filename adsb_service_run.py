404f6647a7b5380731798c2ea221c75732c55149
Junzi Sun <junzi.sun@gmail.com>
2016-08-23 23:29:51 +0200

    update daemon script

--- adsb_service_run.py
+++ adsb_service_run.py
@@ -1,11 +1,11 @@
 import sys
 import os
 import adsb_client
-from daemon import runner
+from daemon.runner import DaemonRunner
 
 f = open(os.devnull, 'w')
 sys.stdout = f
 
 app = adsb_client.Client()
-daemon_runner = runner.DaemonRunner(app)
+daemon_runner = DaemonRunner(app)
 daemon_runner.do_action()
