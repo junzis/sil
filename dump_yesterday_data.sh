29429d3620377b36bb3690fbf9e9e1d78d0f1151
junzis <junzis@users.noreply.github.com>
2017-05-19 14:25:18 +0200

    update cron script

--- dump_yesterday_data.sh
+++ dump_yesterday_data.sh
@@ -13,17 +13,21 @@ mkdir -p ${folder2}
 
 gzip ${datafolder}/ADSB_RAW_${yesterday}.csv
 gzip ${datafolder}/EHS_RAW_${yesterday}.csv
+gzip ${datafolder}/ELS_RAW_${yesterday}.csv
 
 cp ${datafolder}/ADSB_RAW_${yesterday}.csv.gz ${folder}
 cp ${datafolder}/EHS_RAW_${yesterday}.csv.gz ${folder}
+cp ${datafolder}/ELS_RAW_${yesterday}.csv.gz ${folder}
 
 cp ${datafolder}/ADSB_RAW_${yesterday}.csv.gz ${folder2}
 cp ${datafolder}/EHS_RAW_${yesterday}.csv.gz ${folder2}
+cp ${datafolder}/ELS_RAW_${yesterday}.csv.gz ${folder2}
 
 # remove data from one week ago
 olddate=$(date +%Y%m%d -d "yesterday -7 days")
 rm -f ${datafolder}/ADSB_RAW_${olddate}.csv.gz
 rm -f ${datafolder}/EHS_RAW_${olddate}.csv.gz
+rm -f ${datafolder}/ELS_RAW_${olddate}.csv.gz
 
 # delete data on Web dir from 4 months ago, if existing
 oldyear=$(date +%Y -d "-4 month")
