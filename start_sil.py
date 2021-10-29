455830b047b2350b98cae01b233c3c43e890b1fa
Xavier Olive <git@xoolive.org>
2021-10-29 11:34:08 +0200

    add an --output argument

--- start_sil.py
+++ start_sil.py
@@ -39,13 +39,22 @@ parser.add_argument(
     type=int,
 )
 
+parser.add_argument(
+    "--output",
+    dest="output_dir",
+    help="output directory",
+    default=None
+)
+
 parser.add_argument("--debug", dest="debug", action="store_true", default=False)
 
 args = parser.parse_args()
 
 if args.stream_type.lower() == "beast":
     stream = BeastStream(
-        args.host, args.port, df_filter=args.df_filter, buff_size=args.buff_size
+        args.host, args.port, df_filter=args.df_filter,
+        buff_size=args.buff_size,
+        output_dir=args.output_dir
     )
 
 if args.stream_type.lower() == "avr":
